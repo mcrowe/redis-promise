ca00964f25253dcc250db92d7d00f442ec544a84
Mitch Crowe <crowe.mitch@gmail.com>
2017-04-14 13:13:00 -0700

    Release v0.0.1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,12 @@
-declare function doSomething(a: number): number;
-export default doSomething;
+export interface ClientParams {
+    host?: string;
+    port?: number;
+    path?: string;
+    url?: string;
+    password?: string;
+    retry_strategy?: Function;
+}
+declare var _default: {
+    createClient: (params: ClientParams) => any;
+};
+export default _default;
